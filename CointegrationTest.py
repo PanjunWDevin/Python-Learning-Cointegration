6f6d0f60e3727160833abfad32b7117181361598
Panjun <wangpjdevin@gmail.com>
2016-07-26 23:32:04 +0800

    Update CointegrationTest.py

--- CointegrationTest.py
+++ CointegrationTest.py
@@ -1,11 +1,12 @@
-#this is framework for testing cointegration
-#China A shares data package using Tushare
+# this framework provides a way to do pair trading using co-integration
+# for china a shares, data package referred to Tushare
 
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import seaborn as sns
 import tushare as ts
+import matplotlib.pyplot as plt
 
 # input is a DataFrame, Stock's Daily price
 # initialize a functoin to find cointegration
@@ -44,16 +45,65 @@ price_df = ts.get_hist_data('002142',start='2014-01-01',end='2015-01-01')
 prices_df = pd.DataFrame(price_df['close'])
 prices_df.rename(columns = {'close':'002142'},inplace = True)
 
-#print prices_df
-
 for code in stock_list:
     price_df = ts.get_hist_data(code,start='2014-01-01',end='2015-01-01')
     prices_df[code]=price_df['close']
 
 pvalues, pairs = find_cointegration_pairs(prices_df)
 
-stock_list_full = stock_list = ['002142','600000', '600015', '600016', '600036', '601009','601166', '601169', '601328', '601398', '601988', '601998']
-ax = sns.heatmap(1-pvalues,xticklabels=stock_list_full,yticklabels=stock_list_full,cmap='RdYlGn_r',mask = (pvalues==1))
-ax.show()
+#choose the pair with the smallest value
+p_ini = pairs[0][2]
+stock_s1 = pairs[0][0]
+stock_s2 = pairs[0][1]
 
-print pairs
+for item in pairs:
+    if item[2] < p_ini:
+        stock_s1 = item[0]
+        stock_s2 = item[1]
+
+#run the regression on the tested pairs
+
+print(stock_s1)
+print(stock_s2)
+
+x = prices_df[stock_s1]
+y = prices_df[stock_s2]
+
+#X = sm.add_constant(x)
+#result = (sm.OLS(y,X)).fit()
+#print (result.summary())
+
+#beta coefficient 0.179
+
+#fig, ax = plt.subplots(figsize=(8,6))
+#ax.plot(x, y, 'o', label="data")
+#ax.plot(x, result.fittedvalues, 'r', label="OLS")
+#ax.legend(loc='best')
+#plt.show()
+
+#plt.plot(0.6174*x-y)
+#plt.axhline((0.6174*x-y).mean(), color="red", linestyle="--")
+#plt.xlabel("Time"); plt.ylabel("Stationary Series")
+#plt.legend(["Stationary Series", "Mean"])
+#plt.show()
+
+def cal_z_score(series):
+    return (series - series.mean())/np.std(series)
+
+#get z_score series
+z_score_s = cal_z_score(0.179*x - y)
+
+print z_score_s
+
+
+
+#plt.plot(z_score_s)
+#plt.axhline(z_score_s.mean(), color="black")
+#plt.axhline(1.0, color="red", linestyle="--")
+#plt.axhline(-1.0, color="green", linestyle="--")
+#plt.legend(["z-score", "mean", "+1", "-1"])
+#plt.show()
+
+#Back_testing
+#trading strategy implementation
+
