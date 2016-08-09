814899e053bba1616895a739ae258e0b50e604ba
Panjun <wangpjdevin@gmail.com>
2016-08-09 23:48:09 +0800

    Update CointegrationTest.py

--- CointegrationTest.py
+++ CointegrationTest.py
@@ -1,6 +1,4 @@
 # this framework provides a way to do pair trading using co-integration
-# for china a shares, data package referred to Tushare
-
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
@@ -33,23 +31,37 @@ def find_cointegration_pairs(dataframe):
             #get pvale
             pvalue = result[1]
             pvalue_mat[i,j]=pvalue
-            if pvalue < 0.05:
+            if pvalue < 0.02: #only retain very significant cointegrated stocks
                 # record the stock and corresponding p-value
                 pairs.append((keys[i],keys[j],pvalue))
 
     return pvalue_mat, pairs
 
-stock_list = ['600000', '600015', '600016', '600036', '601009','601166', '601169', '601328', '601398', '601988', '601998']
+# find shang zheng 50 stocks
+stock_list = ts.get_sz50s()['code']
 
-price_df = ts.get_hist_data('002142',start='2014-01-01',end='2015-01-01')
-prices_df = pd.DataFrame(price_df['close'])
-prices_df.rename(columns = {'close':'002142'},inplace = True)
+#read data from tushare and store as csv file named stockprices.csv
+#for code in stock_list:
+#    price_df = ts.get_hist_data(code,start='2013-07-01',end='2014-07-01')
+#    prices_df[code]=price_df['close']
 
-for code in stock_list:
-    price_df = ts.get_hist_data(code,start='2014-01-01',end='2015-01-01')
-    prices_df[code]=price_df['close']
+#prices_df.to_csv('/Users/panjunwang/PycharmProjects/PythonWork/PairTradingFrameWork/stockprices.csv')
+
+prices_df = pd.read_csv('/Users/panjunwang/PycharmProjects/PythonWork/PairTradingFrameWork/StockPrices_Pool.csv')
+
+#clean the data, fill up the missing values
+#'601985' all prices are not avaiable so drop this stock
+prices_df = prices_df.drop('601985', 1)
+# fill the missing values by method = pad, fill values forward
+prices_df = prices_df.fillna(method= 'pad')
+# drop the first column 'date'
+prices_df = prices_df.drop('date',1)
+# reverse the order of the prices
+prices_df = prices_df.iloc[::-1]
 
 pvalues, pairs = find_cointegration_pairs(prices_df)
+
+print pairs
 
 #choose the pair with the smallest value
 p_ini = pairs[0][2]
@@ -61,19 +73,26 @@ for item in pairs:
         stock_s1 = item[0]
         stock_s2 = item[1]
 
-#run the regression on the tested pairs
-
 print(stock_s1)
 print(stock_s2)
+
+#run the regression on the tested pairs
+#just to note, we still use stock_s1 = '601800', stock_s1 = '601818'
+stock_s1 = '601800'
+stock_s2 = '601818'
 
 x = prices_df[stock_s1]
 y = prices_df[stock_s2]
 
-#X = sm.add_constant(x)
-#result = (sm.OLS(y,X)).fit()
-#print (result.summary())
+x.plot()
+y.plot()
+plt.show()
 
-#beta coefficient 0.179
+X = sm.add_constant(x)
+result = (sm.OLS(y,X)).fit()
+print (result.summary())
+
+#beta coefficient 0.7745
 
 #fig, ax = plt.subplots(figsize=(8,6))
 #ax.plot(x, y, 'o', label="data")
@@ -87,14 +106,13 @@ y = prices_df[stock_s2]
 #plt.legend(["Stationary Series", "Mean"])
 #plt.show()
 
-def cal_z_score(series):
-    return (series - series.mean())/np.std(series)
+#def cal_z_score(series):
+#    return (series - series.mean())/np.std(series)
 
 #get z_score series
-z_score_s = cal_z_score(0.179*x - y)
+#z_score_s = cal_z_score(0.179*x - y)
 
-print z_score_s
-
+#print z_score_s
 
 
 #plt.plot(z_score_s)
@@ -106,4 +124,3 @@ print z_score_s
 
 #Back_testing
 #trading strategy implementation
-
